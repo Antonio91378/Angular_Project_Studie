3d27d0ffaea608d997b70fcf5ab23865fefcdeae
Antonio Dias <antoniovictor200@gmail.com>
2022-10-13 15:01:36 -0300

    utilizando Reactive Forms customizados que consomem serviços assíncronos

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuario } from './novo-usuario';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { minusculoValidator } from './minusculo.Validator';
+import { UsuarioExisteService } from './usuario-existe.service';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -14,14 +15,19 @@ export class NovoUsuarioComponent implements OnInit {
 
   constructor(
     private formBuilder: FormBuilder,
-    private novoUsuarioService: NovoUsuarioService
+    private novoUsuarioService: NovoUsuarioService,
+    private usuarioExistenteService: UsuarioExisteService
   ) {}
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       fullName: ['', [Validators.required, Validators.minLength(10)]],
-      userName: ['', [minusculoValidator]],
+      userName: [
+        '',
+        [minusculoValidator],
+        [this.usuarioExistenteService.usuarioJaExiste()],
+      ],
       password: ['', [Validators.required]],
     });
   }
